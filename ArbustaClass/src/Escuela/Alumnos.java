efcdd0d4384ed85afc17bd04ae760b564406036d
Arbusta <Arbusta@N-147>
2020-01-15 12:15:20 -0300

    Primer commit clase1

---
+++ ArbustaClass/src/Escuela/Alumnos.java
@@ -0,0 +1,66 @@
+package Escuela;
+
+public class Alumnos {
+private String nombre;
+private String apellido;
+private int legajo;
+private int notademat;
+private int notadeleng;
+private int notadeprog;
+private int sumanotas;
+private float resultado;
+
+public String getNombre() {
+	return nombre;
+}
+public void setNombre(String nombre) {
+	this.nombre = nombre;
+}
+public String getApellido() {
+	return apellido;
+}
+public void setApellido(String apellido) {
+	this.apellido = apellido;
+}
+public int getLegajo() {
+	return legajo;
+}
+public void setLegajo(int legajo) {
+	this.legajo = legajo;
+}
+public int getNotademat() {
+	return notademat;
+}
+public void setNotademat(int notademat) {
+	this.notademat = notademat;
+}
+public int getNotadeleng() {
+	return notadeleng;
+}
+public void setNotadeleng(int notadelen) {
+	this.notadeleng = notadelen;
+}
+public int getNotadeprog() {
+	return notadeprog;
+}
+public void setNotadeprog(int notadeprog) {
+	this.notadeprog = notadeprog;
+}
+public int getSumanotas() {
+	return sumanotas;
+}
+public void setSumanotas(int sumanotas) {
+	this.sumanotas = sumanotas;
+}
+public float getResultado() {
+	return resultado;
+}
+public void setResultado(float resultado) {
+	this.resultado = resultado;
+}
+
+
+
+
+
+}
---
+++ ArbustaClass/src/Notas/Nota_resultado.java
@@ -0,0 +1,13 @@
+package Notas;
+
+import Escuela.Alumnos;
+
+public class Nota_resultado {
+
+	public static void main(String [] args) {
+		
+		 
+	}
+	
+	
+}
